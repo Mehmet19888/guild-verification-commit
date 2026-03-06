8360de94b487f986811339bcf06aaca067e6ac42
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-06 16:18:35 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T13:18:27.085Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T13:18:39.570Z
+Commit: Fix CSS styling bug on mobile devices
