60803210d5dba894fc998763e14be8ba4b3a6b0b
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-06 16:16:57 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T13:15:11.093Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T13:17:01.046Z
+Commit: Patch API endpoint error handling
