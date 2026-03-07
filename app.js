ae89061f3ddd126bff768da6dd4058ce7e5b685f
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:17:00 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:16:55.915Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-07T15:17:04.703Z
+Commit: Fix broken links in documentation
