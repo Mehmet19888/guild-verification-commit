981a36268a97776ffe03d314167473eacd10c448
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:16:42 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:16:42.889Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-07T15:16:47.488Z
+Commit: Fix CSS styling bug on mobile devices
