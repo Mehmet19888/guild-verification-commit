07bb37c593e4e8a6d3c7986037594c5e2d31fcba
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:17:22 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:16:30.107Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-07T15:17:26.664Z
+Commit: Correct typo in configuration file
