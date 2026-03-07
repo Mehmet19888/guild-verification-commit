ba0f00c4e97e8681aea90fb6fbe4b68463c3663a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:30:06 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:29:59.780Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-07T15:30:11.406Z
+Commit: Fix memory leak in event handlers
