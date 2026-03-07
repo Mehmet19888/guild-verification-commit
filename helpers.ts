8ed586d7756435144cc845892507a60907ec7f19
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:04:43 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:04:38.831Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-07T15:04:48.320Z
+Commit: Correct typo in configuration file
