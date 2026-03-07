7341eeb96947e38900d9e6082180628bf68727bd
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:30:29 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:30:26.253Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-07T15:30:34.378Z
+Commit: Add dark mode support
