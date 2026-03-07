19d8b234146079d004bc7ff62462c6a795bf100e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:32:28 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:31:56.658Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-07T15:32:33.218Z
+Commit: Fix CSS styling bug on mobile devices
