8b641f298ec0d2bc5a92ece95a79458b3bbf17c2
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:28:41 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:28:42.200Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-07T15:28:46.437Z
+Commit: Fix memory leak in event handlers
