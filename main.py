86b1256764966fd5d14287bec55ac7e9ff0da33e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:03:21 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:03:07.471Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-07T15:03:26.100Z
+Commit: Introduce real-time notifications
