6deeecff951d90b06fb202301876d1f9edb6fec7
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:07:12 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:07:15.499Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-07T15:07:16.903Z
+Commit: Add export functionality for reports
