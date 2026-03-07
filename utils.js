4fbd0798710eac84be7af47b2558542f2c892964
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:29:28 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:29:01.855Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T15:29:32.761Z
+Commit: Fix broken links in documentation
