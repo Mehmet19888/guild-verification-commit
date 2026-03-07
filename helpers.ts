d01ff983cfe1ed37f039b79c5fdb4f65ade81fce
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 17:59:46 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T14:59:23.444Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-07T14:59:50.929Z
+Commit: Resolve null pointer exception in user service
