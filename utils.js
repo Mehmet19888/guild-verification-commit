e55b2d55f9344a5133daa32c26266e6ce44731a3
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:21:51 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:21:53.753Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-07T15:21:55.745Z
+Commit: Fix broken links in documentation
