3021a329b811d1d7a0bdc5176d7cb537eaa21556
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:07:30 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:07:25.471Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-07T15:07:34.578Z
+Commit: Fix broken links in documentation
