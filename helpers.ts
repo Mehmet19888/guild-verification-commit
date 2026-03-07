c795d4de7cd6da540632a9c94e89242ccca58912
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:19:42 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:19:44.393Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-07T15:19:47.149Z
+Commit: Patch API endpoint error handling
