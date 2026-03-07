dfd8d71141dc862febf14ceda2fb949feab9f3e8
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:25:53 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:25:57.145Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-07T15:25:58.464Z
+Commit: Fix broken links in documentation
