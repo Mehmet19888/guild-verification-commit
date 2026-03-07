c03ec42832e6d6ddcaaab7a944dac4a191a1156b
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:24:18 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:24:05.566Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-07T15:24:22.538Z
+Commit: Resolve null pointer exception in user service
