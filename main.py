37387f72cc4dc175af6f5c91aa355ebd810a8031
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-07 18:06:01 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-07T15:05:59.399Z
-Commit: Add multi-language support
+Timestamp: 2026-03-07T15:06:06.318Z
+Commit: Resolve timezone issues in date handling
