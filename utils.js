54a42f160e193a706e1e7e1c16a44a5fae5a32f6
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-08 17:22:24 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T14:22:10.285Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-08T14:22:24.863Z
+Commit: Resolve null pointer exception in user service
