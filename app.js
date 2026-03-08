c680c1ff253f56eed002c38ebf35387e01307c03
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-08 17:19:36 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T14:19:24.988Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T14:19:36.634Z
+Commit: Resolve null pointer exception in user service
