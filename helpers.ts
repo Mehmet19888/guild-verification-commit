5faa64ce0970d64db3e037573a3ad360297a3c3a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:32:23 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:31:59.698Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-10T12:32:23.866Z
+Commit: Fix memory leak in event handlers
