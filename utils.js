2f44e6a78a87f93aa8b7614d6a89a2ded4f43d0d
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:43:37 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:34:48.328Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-10T12:43:38.245Z
+Commit: Fix broken links in documentation
