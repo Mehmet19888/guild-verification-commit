67ee4c5c96f5ccef62a4e8f4af381e2710ef0ced
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:33:03 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:32:59.058Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-10T12:33:04.690Z
+Commit: Fix race condition in async operations
