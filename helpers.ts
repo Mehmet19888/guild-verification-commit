dffd1a51f1e265d10909205cbfbeb18bdacb4263
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:33:08 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:33:07.498Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-10T12:33:09.121Z
+Commit: Resolve timezone issues in date handling
