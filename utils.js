ef848345a1eedacdfb7adae6cd08342a0f96f249
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:32:58 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:32:27.127Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-10T12:32:59.058Z
+Commit: Patch API endpoint error handling
