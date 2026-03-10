df4471583d930dc219fde9855ee63a0e6498477d
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 16:08:14 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T13:07:25.835Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-10T13:08:13.036Z
+Commit: Fix broken links in documentation
