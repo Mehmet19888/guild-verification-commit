e6dd97db9648823e11a3937b1df33679cc7ee143
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:32:00 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T14:22:21.832Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-10T12:32:01.428Z
+Commit: Introduce real-time notifications
