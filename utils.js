9991248eeddb71f782f97de5451b8c8fa58066d7
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-10 15:32:26 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-10T12:32:10.810Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-10T12:32:27.127Z
+Commit: Add export functionality for reports
