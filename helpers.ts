b1761abd2d750da10c3f5f2546efd4771d86cce6
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-12 20:50:27 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:50:02.764Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T17:50:29.656Z
+Commit: Patch API endpoint error handling
