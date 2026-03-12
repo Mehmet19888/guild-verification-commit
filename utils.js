5b0741143044267fb63b782400fac5eacd5261ca
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-12 20:51:17 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:50:52.347Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-12T17:51:19.566Z
+Commit: Patch API endpoint error handling
