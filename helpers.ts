dc2eeecd157f480ec2a3ec445a9decdb8951cb00
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-12 20:50:39 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:50:29.656Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T17:50:41.603Z
+Commit: Fix CSS styling bug on mobile devices
