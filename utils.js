09a57c39391619367d733dad4dcadca069e8eb10
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-12 20:53:07 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T17:52:39.725Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-12T17:53:09.946Z
+Commit: Fix race condition in async operations
