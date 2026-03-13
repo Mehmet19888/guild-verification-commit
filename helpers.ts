2dc55316ea3b8705ee658175510bc7b497bd2e35
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 19:44:15 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T16:43:58.972Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-13T16:44:17.178Z
+Commit: Fix memory leak in event handlers
