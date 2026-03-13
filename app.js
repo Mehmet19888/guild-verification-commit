a9e473e5d23c065e3a69f75538ad37e90789b62e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 16:52:05 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T13:42:36.737Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-13T13:52:07.532Z
+Commit: Fix CSS styling bug on mobile devices
