7ea12518bb182adc754cb42caa777868b03b2b82
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 18:18:32 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T15:15:03.771Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-13T15:18:34.956Z
+Commit: Patch API endpoint error handling
