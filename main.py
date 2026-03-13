1d00bec2ab195bd94c7f76f3fadd442274bcf059
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 15:36:35 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T12:29:40.388Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-13T12:36:37.230Z
+Commit: Add multi-language support
