6d5150acc4ab21a23ed1826a99d7d740ef4fc741
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 21:48:46 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T18:48:47.238Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-13T18:48:48.638Z
+Commit: Fix memory leak in event handlers
