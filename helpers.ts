70083656b201d3f298b4cc86efeee193ec0e3718
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 15:55:55 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T12:54:22.051Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-13T12:55:57.073Z
+Commit: Resolve null pointer exception in user service
