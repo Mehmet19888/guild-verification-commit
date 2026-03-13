6b12c3153a9d9b073fae2821db31310012f66f97
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 19:46:39 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T16:46:09.307Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-13T16:46:42.222Z
+Commit: Fix memory leak in event handlers
