0a4b4ea75208179ed9ccd7d28718eef75c45a2cc
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 19:33:46 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T16:32:34.601Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-13T16:33:48.940Z
+Commit: Correct typo in configuration file
