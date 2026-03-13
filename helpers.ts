48d6542e3123642f3096a8e36caef2d516acee57
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 19:43:56 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T16:43:48.989Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-13T16:43:58.972Z
+Commit: Fix CSS styling bug on mobile devices
