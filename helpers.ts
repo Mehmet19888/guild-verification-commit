ac61d2f88df4ad6b87479bea7ce0776b031c5b16
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 16:51:55 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T13:42:39.501Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-13T13:51:57.714Z
+Commit: Resolve null pointer exception in user service
