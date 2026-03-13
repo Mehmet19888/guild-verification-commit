0323fd630ec32eafa41c315e677485415b94b566
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 16:21:54 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T13:21:21.306Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-13T13:21:56.626Z
+Commit: Fix broken links in documentation
