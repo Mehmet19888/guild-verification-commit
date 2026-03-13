e75aed1f178310100f2f25233ea03e62a5a91b00
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 21:46:05 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T18:46:00.623Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-13T18:46:07.696Z
+Commit: Fix race condition in async operations
