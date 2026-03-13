e73a32dfd3177ae3b0bea43ed17197b9c2188880
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 15:11:52 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T12:11:52.225Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-13T12:11:54.336Z
+Commit: Resolve timezone issues in date handling
