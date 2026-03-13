e92013457b89d54542a15199f30409bda26bc7c1
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 16:55:56 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T13:55:28.931Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-13T13:55:58.903Z
+Commit: Fix CSS styling bug on mobile devices
