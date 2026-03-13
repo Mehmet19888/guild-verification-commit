c6b5c0fe8f8464a9443a26102ae517d685a2b83e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 21:52:20 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T18:51:35.636Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-13T18:52:22.214Z
+Commit: Resolve null pointer exception in user service
