16e24dea2ccfef51e5803a2c75ab25e281ab6426
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 21:49:46 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T18:49:47.040Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-13T18:49:48.405Z
+Commit: Resolve null pointer exception in user service
