693f6cd17a1e75296354c02e9209e5b7c8c82c3e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-13 19:45:42 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T16:44:21.914Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-13T16:45:44.595Z
+Commit: Fix CSS styling bug on mobile devices
