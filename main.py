2a0d49c20882c894e2fbb59b6c4f5612bcedc902
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:57:26 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:22.321Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T22:57:24.251Z
+Commit: Introduce real-time notifications
