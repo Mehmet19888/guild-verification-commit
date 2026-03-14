1912fb2951831c4490d5669670fe3a918b908375
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-14 17:32:52 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T14:32:43.841Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-14T14:32:54.004Z
+Commit: Patch API endpoint error handling
