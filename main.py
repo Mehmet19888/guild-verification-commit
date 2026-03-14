fe0ea656c4337b92c1f3fb6e21a9d9614c1dde93
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:55:07 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:53:40.764Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T22:55:04.790Z
+Commit: Resolve timezone issues in date handling
