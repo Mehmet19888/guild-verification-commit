e097ed13785d3aedb2b4c79e404ade589f841fc4
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:57:17 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:07.708Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-14T22:57:14.785Z
+Commit: Patch API endpoint error handling
