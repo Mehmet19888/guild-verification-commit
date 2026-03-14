af5d3ba26daf8754061a14879281fd3c3bb16f48
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-14 17:42:45 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T14:42:44.894Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T14:42:47.579Z
+Commit: Fix broken links in documentation
