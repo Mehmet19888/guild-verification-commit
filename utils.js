579396dc044a326185460298a1d98d5b0d4dccf7
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-14 17:59:26 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T14:53:32.916Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-14T14:59:25.127Z
+Commit: Resolve null pointer exception in user service
