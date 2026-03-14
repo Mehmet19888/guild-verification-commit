dcbb1203a5b12a0a2e145e89d22a159ba9a6f452
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-14 18:00:54 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T15:00:54.919Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T15:00:56.399Z
+Commit: Fix broken links in documentation
