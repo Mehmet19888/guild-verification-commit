d6dc44dce987abab8e42fd005c2ad034dad3f6c9
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:57:13 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:05.664Z
-Commit: Add multi-language support
+Timestamp: 2026-03-14T22:57:11.150Z
+Commit: Resolve timezone issues in date handling
