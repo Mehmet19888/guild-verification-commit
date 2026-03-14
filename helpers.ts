411c5779a1f7a45cb0b6cff118ebf4dc8da6913e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:54:29 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:54:13.931Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T22:54:27.912Z
+Commit: Introduce real-time notifications
