517f277f031776a52597fea181677f1da349f7cb
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:55:38 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:55:08.899Z
-Commit: Add dark mode support
+Timestamp: 2026-03-14T22:55:36.568Z
+Commit: Resolve null pointer exception in user service
