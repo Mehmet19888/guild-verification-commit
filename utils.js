a7f20475adabef8736ec25a714c63b09d8796989
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:53:58 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:53:38.742Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-14T22:53:56.433Z
+Commit: Fix broken links in documentation
