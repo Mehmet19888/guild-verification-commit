f8b11579f8dd7fe5cb34fb0da50378268250e28b
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:57:24 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:00.261Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T22:57:22.321Z
+Commit: Resolve null pointer exception in user service
