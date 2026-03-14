993426b8de3745d750cbf2390e2bff522a089719
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:48:47 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:48:18.396Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T22:48:45.711Z
+Commit: Fix broken links in documentation
