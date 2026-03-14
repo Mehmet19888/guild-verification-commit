b8b495f123dfa3b51c1d0a1ad161aa2e9f39b38c
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:47:49 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:47:20.442Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-14T22:47:47.225Z
+Commit: Patch security vulnerability in authentication
