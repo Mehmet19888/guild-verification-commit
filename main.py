ff2660aaab3045e6dc249eca5cd0580def512d87
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-14 17:26:13 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T14:25:53.739Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T14:26:15.528Z
+Commit: Fix memory leak in event handlers
