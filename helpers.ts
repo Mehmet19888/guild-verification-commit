d6213758ea187eea7e1823667829adeb5033349f
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:53:07 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:52:57.763Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T22:53:05.499Z
+Commit: Patch security vulnerability in authentication
