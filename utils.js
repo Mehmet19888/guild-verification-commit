8f39134590db7a2ec146e0f9fe93b595f2375d80
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 01:58:02 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:57:56.838Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T22:58:00.788Z
+Commit: Add export functionality for reports
