ce07c9e735f7e4385ee547d4a7274e8a11e448d5
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 11:59:30 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T08:59:24.919Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-15T08:59:28.427Z
+Commit: Fix CSS styling bug on mobile devices
