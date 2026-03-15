a19ae6520fe9088ad61590a8551cb1f511546bbc
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 19:18:57 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T16:13:45.475Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-15T16:18:57.829Z
+Commit: Fix broken links in documentation
