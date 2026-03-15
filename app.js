bfdc804886230494a7ac48a4c8d703afe67a686f
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 18:08:44 +0300

    Patch API endpoint error handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T15:08:26.273Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T15:08:42.909Z
+Commit: Patch API endpoint error handling
