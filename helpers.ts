a316d020da8f5f6d6c331841d0319afc8afe5e9d
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 11:57:06 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T08:56:39.933Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-15T08:57:04.267Z
+Commit: Fix broken links in documentation
