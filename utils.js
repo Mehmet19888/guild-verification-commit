04f92ad91febed7b2f0fe2ff03b8fafe7b9c74b2
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:52:27 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:51:56.644Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-15T10:52:24.854Z
+Commit: Introduce real-time notifications
