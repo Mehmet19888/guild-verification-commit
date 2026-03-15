d613b6d895dee59ec2320c48bad623a12331833b
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 11:59:10 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T08:58:54.946Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-15T08:59:08.352Z
+Commit: Add multi-language support
