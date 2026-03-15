cb80563106adc4f2f46c6d9bc7ce4bff88921958
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:49:14 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:48:44.376Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-15T10:49:11.709Z
+Commit: Add dark mode support
