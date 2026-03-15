7b6e7cd2a12626b2b47ac4706fcd67d1052442b9
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 18:10:37 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T15:09:05.894Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-15T15:10:36.191Z
+Commit: Fix broken links in documentation
