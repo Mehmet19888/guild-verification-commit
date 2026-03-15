c4d881195dcf474c3044c05d7c2a6c9eef1b27d2
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 18:08:42 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T15:08:39.148Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-15T15:08:41.230Z
+Commit: Resolve null pointer exception in user service
