30e6b2876ffdaf5dd2514d0584a01bcf7f3a7c71
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:45:00 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:44:48.567Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-15T10:44:57.909Z
+Commit: Fix broken links in documentation
