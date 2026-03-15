80bd186094004dee231a94c203f99f5adae32837
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 15:58:52 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T12:58:35.954Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-15T12:58:51.338Z
+Commit: Patch API endpoint error handling
