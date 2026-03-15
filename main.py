baa6e8930491a8c20e2425ab6be7b9cfea2ddaac
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:46:27 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:46:12.603Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-15T10:46:24.748Z
+Commit: Fix memory leak in event handlers
