6d3e6f0dfbdae5fe1374460118a8543a37146d66
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:51:59 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:51:50.484Z
-Commit: Add multi-language support
+Timestamp: 2026-03-15T10:51:56.644Z
+Commit: Resolve null pointer exception in user service
