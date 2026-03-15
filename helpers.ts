e24d659595225f11f70a39d42f7856da07542d78
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 13:51:34 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T10:51:30.584Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T10:51:32.409Z
+Commit: Resolve null pointer exception in user service
