b60ce65ef5b2f57c44af6000c7df22cac80a7c98
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 11:56:52 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:58:17.646Z
-Commit: Add multi-language support
+Timestamp: 2026-03-15T08:56:50.749Z
+Commit: Resolve null pointer exception in user service
