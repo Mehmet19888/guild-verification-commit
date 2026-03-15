0be78c8c6da665ae81ffe0bca442f105cf689a42
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-15 11:55:13 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:58:25.188Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T08:55:11.567Z
+Commit: Resolve null pointer exception in user service
