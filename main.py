80656c2ed8ffb0865476bfcb47804c18a12ad0c7
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 21:07:54 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T18:07:29.358Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-16T18:07:52.589Z
+Commit: Resolve null pointer exception in user service
