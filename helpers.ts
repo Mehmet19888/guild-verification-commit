cd104d72bad98fad00145069c7ce3381638fc4ca
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 10:47:48 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T07:19:33.347Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-16T07:47:47.816Z
+Commit: Patch security vulnerability in authentication
