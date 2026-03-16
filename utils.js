c035c78a20904b94e840115a8fe5fde4db435c21
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 10:48:04 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T07:19:31.525Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-16T07:48:04.066Z
+Commit: Fix CSS styling bug on mobile devices
