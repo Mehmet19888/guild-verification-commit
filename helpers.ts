2be0a15f1c4fb1481b1993a3187480c6806cc5e3
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 21:10:24 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T18:09:43.852Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-16T18:10:22.574Z
+Commit: Fix CSS styling bug on mobile devices
