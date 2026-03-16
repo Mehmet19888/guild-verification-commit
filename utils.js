039a65517f8788fbaf5f0174ebce7688bcbedd9c
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 10:19:02 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T07:18:53.898Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-16T07:19:01.738Z
+Commit: Fix broken links in documentation
