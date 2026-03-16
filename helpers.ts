a4f48b490b872cf8e654084f5289c927a1039610
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-16 21:07:48 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-16T18:05:00.431Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-16T18:07:46.966Z
+Commit: Correct typo in configuration file
