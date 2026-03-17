b562002d7eca9db2aff5f1076530890dea9d4a59
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-17 11:09:18 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-17T08:07:38.940Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-17T08:09:17.868Z
+Commit: Fix broken links in documentation
