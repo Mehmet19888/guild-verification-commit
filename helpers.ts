12f753bce6465baca301c5df56d48d12349c89de
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-18 16:26:59 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T13:26:06.871Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-18T13:26:59.182Z
+Commit: Patch API endpoint error handling
