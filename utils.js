7283637c700c6ef66f3c1de156fe18ba8ad46e54
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-18 16:29:52 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T13:29:43.281Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-18T13:29:52.215Z
+Commit: Fix broken links in documentation
