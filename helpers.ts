a7e20749e19e6d2de6a05d59d76c0d91c88c50f4
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-18 16:28:12 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T13:28:00.894Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-18T13:28:11.915Z
+Commit: Introduce real-time notifications
