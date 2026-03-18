d336dc786e75a991237d570aa63a261a5dea1398
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-18 16:25:33 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T13:24:38.176Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-18T13:25:33.109Z
+Commit: Fix race condition in async operations
