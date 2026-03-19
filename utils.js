d371cdf7106e7f8a24ba049c2b6a580a0b019d32
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 16:50:07 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:49:31.375Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-19T13:50:06.168Z
+Commit: Resolve timezone issues in date handling
