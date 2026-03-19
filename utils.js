f2320d2cde1f411c69c80abb5b9a5c611da98f62
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 16:52:18 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:52:02.618Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-19T13:52:17.364Z
+Commit: Fix memory leak in event handlers
