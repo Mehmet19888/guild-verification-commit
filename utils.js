4f024b7ca88c8b5042776cbe5afb73e2b63c37cd
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:02:57 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:02:04.865Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-19T14:02:56.391Z
+Commit: Add multi-language support
