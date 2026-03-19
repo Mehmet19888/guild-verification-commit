038f1f9db1f5186033bddb880654386939dac4b9
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:00:31 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:59:45.778Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-19T14:00:30.891Z
+Commit: Resolve timezone issues in date handling
