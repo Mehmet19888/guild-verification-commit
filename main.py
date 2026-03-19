1e4a5a608f4668ad6b8a2602296b2afc178b29de
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 16:49:59 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:49:48.851Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-19T13:49:58.342Z
+Commit: Patch API endpoint error handling
