d63c7bdd2f2f6ce6a40a17c67f98bdfd00430deb
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:03:01 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:02:51.298Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-19T14:03:00.560Z
+Commit: Add multi-language support
