a8a31292e4cc24616168f26b2a8bff3a8b426817
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:03:15 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:03:00.560Z
-Commit: Add multi-language support
+Timestamp: 2026-03-19T14:03:14.064Z
+Commit: Patch security vulnerability in authentication
