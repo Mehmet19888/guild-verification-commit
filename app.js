3ddb69d235ece6d28de8b29109f21deab5db30fc
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:12:50 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:12:28.013Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-19T14:12:49.116Z
+Commit: Fix broken links in documentation
