e8f45606b4ca37430845959e8ad02155d73c5d25
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 16:52:03 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:51:48.709Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-19T13:52:02.618Z
+Commit: Fix CSS styling bug on mobile devices
