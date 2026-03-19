a4409568ecff5e34f0554ed08f014396c7d7bf85
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:12:29 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:12:00.856Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-19T14:12:28.013Z
+Commit: Fix memory leak in event handlers
