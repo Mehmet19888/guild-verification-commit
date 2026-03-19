58fc7ae53234cf013cff8528ad361150123039cb
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 17:04:43 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T14:04:01.954Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-19T14:04:41.937Z
+Commit: Fix CSS styling bug on mobile devices
