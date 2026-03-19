099999f31cc01b439f347987ca0412998eb8f72a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-19 16:57:23 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T13:57:19.793Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-19T13:57:22.383Z
+Commit: Patch security vulnerability in authentication
