ba433f1a5747511fcd3489067a09e796179b1700
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-21 09:45:21 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-21T06:44:22.668Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-21T06:45:19.755Z
+Commit: Fix broken links in documentation
