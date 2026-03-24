d3a6ad984058ff5de7e4123b8faa7dc16a18f97a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 17:49:54 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T14:49:14.330Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-24T14:49:52.209Z
+Commit: Add export functionality for reports
