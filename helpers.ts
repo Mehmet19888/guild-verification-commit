56371a96ac3c5623272f14a8cc3c4d00c4cd0598
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:03:49 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:03:26.314Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T17:03:47.861Z
+Commit: Patch security vulnerability in authentication
