fed2e0e209366c25c7b76e4c2edd74a152ca1d86
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:00:38 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:59:51.466Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T17:00:36.662Z
+Commit: Patch API endpoint error handling
