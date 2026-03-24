2eab115a0c1b6d5c5f5c8e294fe090f120d03655
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:53:19 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:52:49.601Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-24T16:53:18.219Z
+Commit: Resolve null pointer exception in user service
