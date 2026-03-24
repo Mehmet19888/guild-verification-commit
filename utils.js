6645473b2fed83bde1fe855a474bdade3c54978a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:56:49 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:56:45.751Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-24T16:56:48.521Z
+Commit: Fix memory leak in event handlers
