59471bc964510aa5db20ab62bda5457e44fef216
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:03:25 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:03:21.811Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-24T17:03:24.110Z
+Commit: Fix memory leak in event handlers
