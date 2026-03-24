6bc1aa8d3984f5e59c3e43f71382eef88414672d
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:49:32 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:49:17.163Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-24T16:49:31.146Z
+Commit: Patch API endpoint error handling
