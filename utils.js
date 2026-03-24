4b12454987e9391f64b31b71da5115c3d16e7baa
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:55:51 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:55:36.168Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-24T16:55:50.299Z
+Commit: Resolve timezone issues in date handling
