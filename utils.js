b7ef4fc0af336f07985ee1fecebaab2bd357447e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:36:28 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:34:41.846Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-24T17:36:27.482Z
+Commit: Add multi-language support
