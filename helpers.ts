e5204becff526d3700ad4bc43dabfd5a1d3b4eb0
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:04:54 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:04:45.094Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-24T17:04:53.772Z
+Commit: Add multi-language support
