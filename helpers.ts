49606abd8f935df2346e255a398300e651dc9f05
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:04:46 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:04:26.492Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-24T17:04:45.094Z
+Commit: Patch security vulnerability in authentication
