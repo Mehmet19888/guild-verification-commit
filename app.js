3079e210a95d676a64a10627065ecc44a59c990c
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:53:35 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:53:32.169Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-24T16:53:34.294Z
+Commit: Fix memory leak in event handlers
