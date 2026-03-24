a3fa4ea69b9083e547dd522a1be271f679d8e68a
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:43:03 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:42:55.268Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T17:43:02.295Z
+Commit: Resolve timezone issues in date handling
