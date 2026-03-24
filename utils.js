7f721b38ec2d76c3a5837596a56d13a8e8c378ce
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:50:50 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:50:22.248Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-24T16:50:49.462Z
+Commit: Fix broken links in documentation
