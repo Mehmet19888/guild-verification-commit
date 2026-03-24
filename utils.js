5b3603d275cdab31b209b17f4e1a29d21a9ff25f
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:56:41 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:56:35.936Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-24T16:56:40.549Z
+Commit: Resolve timezone issues in date handling
