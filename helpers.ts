7ca50d61bcfeb349e10875e3cc8e1d8341821ba8
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:09:48 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:09:35.047Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-24T17:09:47.106Z
+Commit: Correct typo in configuration file
