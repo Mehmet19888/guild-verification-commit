1954ab13fb9780481ab290d3e509939a8ea4cb65
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:53:33 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:53:01.185Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T16:53:32.169Z
+Commit: Fix broken links in documentation
