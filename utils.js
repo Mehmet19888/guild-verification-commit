1f6595529a905be8217004577ce6faca37c76e07
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:56:36 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:56:07.337Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-24T16:56:35.936Z
+Commit: Introduce real-time notifications
