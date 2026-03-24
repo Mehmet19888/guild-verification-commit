d8d91c6a31a1323be4748eb66de7c2ab976997e9
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:47:28 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T14:53:52.242Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-24T16:47:27.150Z
+Commit: Fix memory leak in event handlers
