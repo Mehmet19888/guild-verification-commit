197ec75d95da4b3ab89e920c007c34dff51940c6
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:08:52 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:08:13.664Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-24T17:08:51.852Z
+Commit: Fix race condition in async operations
