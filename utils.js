c3257d4fbde2fb9ffb2575708bf625c5f6055b35
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 20:17:18 +0300

    Add dark mode support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:15:52.175Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-24T17:17:17.358Z
+Commit: Add dark mode support
