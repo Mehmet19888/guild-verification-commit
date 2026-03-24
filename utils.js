fcd5495ed6113652418d3bf124778615a8488c5e
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 17:50:31 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T14:49:52.209Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-24T14:50:29.902Z
+Commit: Fix memory leak in event handlers
