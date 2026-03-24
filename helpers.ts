39320e5260097b7b3b2d98e3100979a06b5eb1d7
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-24 19:53:17 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T16:52:57.066Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-24T16:53:15.938Z
+Commit: Implement user profile customization
