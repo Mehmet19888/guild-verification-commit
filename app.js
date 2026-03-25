b9a1ad84070bd86a412c15854407a81a4eaf86c2
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-25 13:16:49 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T17:45:34.165Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-25T10:16:47.722Z
+Commit: Fix broken links in documentation
