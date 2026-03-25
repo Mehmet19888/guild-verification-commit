d514515d33548e4f0989d6045df768de624bedfc
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-25 18:45:28 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T15:41:52.467Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-25T15:45:26.988Z
+Commit: Patch API endpoint error handling
