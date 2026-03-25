161e13bf08b989c059531c3b9a12b59a83a7d2c0
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-25 19:16:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T16:14:50.753Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-25T16:16:13.221Z
+Commit: Fix broken links in documentation
