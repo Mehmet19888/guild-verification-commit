d241ff235dc4152ec5bda50fa453443d13535f46
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-25 18:43:16 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-25T15:41:50.425Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-25T15:43:15.346Z
+Commit: Fix CSS styling bug on mobile devices
