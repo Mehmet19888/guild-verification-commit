fe0be5c05aede3b90be2c0f3837c57203a3a62b5
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-26 15:53:32 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T12:52:50.407Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-26T12:53:31.574Z
+Commit: Add multi-language support
