e1928d53d2c03e07fcd56490604f11d1aeb7aac0
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-26 15:53:48 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T12:53:39.284Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-26T12:53:47.923Z
+Commit: Patch security vulnerability in authentication
