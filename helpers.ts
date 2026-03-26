3504f61bf02d68c040b5af25b633a667e138b783
Mehmet19888 <119633978+Mehmet19888@users.noreply.github.com>
2026-03-26 15:52:49 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T12:52:02.218Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-26T12:52:48.428Z
+Commit: Patch API endpoint error handling
